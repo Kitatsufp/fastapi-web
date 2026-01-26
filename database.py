f77893da643a9716a97ab805bb2ea4dffa84e123
Kitatsufp <dattantruongfp@gmail.com>
2026-01-26 15:43:54 +0700

    fix database connection

--- database.py
+++ database.py
@@ -3,17 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://fastapi_user:password@host:5432/fastapi_db"
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
-engine = create_engine(DATABASE_URL, connect_args={
-                       "check_same_thread": False})
-
-
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
 
