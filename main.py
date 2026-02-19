57a7af74c8881e5141803ad7d42f127537cdcbea
Kitatsufp <dattantruongfp@gmail.com>
2026-02-19 11:27:53 +0700

    Add backend code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 from routers import blog, user, authentication
+from fastapi.middleware.cors import CORSMiddleware
 import models
 
 app = FastAPI()
@@ -10,3 +11,10 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # hoặc ["https://your-vercel-app.vercel.app"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
