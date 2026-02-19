85c982dab83d9107d5705f971ceddfd865aa0e3d
Kitatsufp <dattantruongfp@gmail.com>
2026-02-19 16:19:14 +0700

    Add backend code

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://air-quality-dashboard-eta.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
