d8293184ab5166ecac205c4e2b714f8dbd7afdd6
Kitatsufp <dattantruongfp@gmail.com>
2026-04-19 15:48:06 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import engine
-from routers import blog, user, authentication
+from routers import blog, user, authentication, prediction
 from fastapi.middleware.cors import CORSMiddleware
 import models
 
@@ -11,6 +11,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+app.include_router(prediction.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://air-quality-dashboard-eta.vercel.app"],
