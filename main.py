9ecfcdac759adeec65e223d8dcf6ca0d450be0c5
Kitatsufp <dattantruongfp@gmail.com>
2026-05-05 10:29:02 +0700

    Fix: Use environment variables for security

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ app.include_router(user.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*",
         "https://air-quality-dashboard-eta.vercel.app",
         "http://localhost:3000",
         "http://localhost:8080",
